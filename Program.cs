ed41ea7b9162e52444b22b9e1a3a6c2bf7819812
JosephParraT <Josephpt2004@outlook.com>
2024-12-08 11:21:37 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace yoyo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ calseyoyo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yoyo
+{
+    public class calseyoyo
+    {
+        public void WriteLine()
+        {
+            Console.WriteLine("Hola hola");
+        }
+    }
+}
